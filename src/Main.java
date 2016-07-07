0ec768e81ee4ea925ef20017d44743231655975b
ak11234 <akpc556@gmail.com>
2016-07-07 13:27:49 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Adam Kalman on 7/7/2016. The TBD's stock market game
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
