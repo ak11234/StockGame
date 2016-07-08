b83c1b9f54e3ca68921e2daac0a5394c2961ef65
Joshua <joshuagross4444@gmail.com>
2016-07-08 10:20:04 -0500

    Math

--- src/Stock.java
+++ src/Stock.java
@@ -1,3 +1,4 @@
+import org.apache.commons.math3.distribution.NormalDistribution;
 /**
  * Created by Adam on 7/8/2016. Stock object
  */
@@ -12,6 +13,13 @@ public class Stock {
         symbol=newSymbol;
         startPrice=newStartPrice;
     }
-
-
+    
+    public double getPrice(){
+        return currentPrice;
+    }
+    
+    public double changePrice(){
+        return currentPrice*(1+NormalDistribution(0.1/230, 0.2/(Math.sqrt(230))
+        *(1+NormalDistribution(0.1*meanReturn/230, 0.2*std/(Math.sqrt(230))))));
+    }
 }
