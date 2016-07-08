b42c18f545756195f94834ad9004274a95d14048
Joshua <joshuagross4444@gmail.com>
2016-07-08 10:37:56 -0500

    change sd

--- src/Stock.java
+++ src/Stock.java
@@ -6,11 +6,10 @@ import org.apache.commons.math3.distribution.NormalDistribution;
 public class Stock {
     private int index;
     private String symbol;
-    private double std, meanReturn;
+    private double sd, meanReturn;
     private double startPrice, currentPrice, changePrice, changePercent;
 
     public Stock(String newSymbol, double newStartPrice){
-
         symbol=newSymbol;
         startPrice=newStartPrice;
     }
@@ -21,10 +20,16 @@ public class Stock {
     
     public void changePrice(){
         NormalDistribution distMarket= new NormalDistribution(0.1/230, 0.2/(Math.sqrt(230)));
-        NormalDistribution distMe=new NormalDistribution(0.1*meanReturn/230, 0.2*std/(Math.sqrt(230)));
+        NormalDistribution distMe=new NormalDistribution(0.1*meanReturn/230, 0.2*sd/(Math.sqrt(230)));
         double market = Double.parseDouble(distMarket.toString());
         double me = Double.parseDouble(distMarket.toString());
         double newPrice = currentPrice*(1+market)*(1+me);
         currentPrice=newPrice;
     }
+    
+    public String getSymbol(){
+        return symbol;
+    }
+    
+    
 }
