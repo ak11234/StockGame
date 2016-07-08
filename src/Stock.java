726ef6c18f44b5d80a97e9f43f305b5f4848ac2e
ak11234 <akpc556@gmail.com>
2016-07-08 10:02:20 -0500

    made stock class

--- src/Stock.java
+++ src/Stock.java
@@ -1,6 +1,15 @@
 /**
- * Created by Adam on 7/8/2016.
+ * Created by Adam on 7/8/2016. Stock object
  */
 public class Stock {
+    private int index;
+    private String symbol;
+    private double std, meanReturn;
+    private double startPrice, currentPrice, changePrice, changePercent;
+
+    public Stock(String symbol, double startPrice){
+
+    }
+
 
 }
