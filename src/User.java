25e360332990aebe20617890b0f99abd59f28102
Joshua <joshuagross4444@gmail.com>
2016-07-08 11:43:01 -0500

    added stuff

---
+++ src/User.java
@@ -0,0 +1,23 @@
+
+/**
+ * The user class
+ * 
+ * @author (Josh Gross) 
+ */
+public class User
+{
+    private double myNetworth;
+    private double myCash;
+    
+    
+    
+    /**
+     * Constructor for objects of class User
+     */
+    public User()
+    {
+        // initialise instance variables
+        
+    }
+
+}
