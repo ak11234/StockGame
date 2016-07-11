59fa2bb603e3733582eaf83a8ff3425c19b1a21d
Joshua <joshuagross4444@gmail.com>
2016-07-11 11:14:07 -0500

    stuff

--- src/Update.java
+++ src/Update.java
@@ -2,10 +2,6 @@
  * Created by Adam on 7/8/2016. To update
  */
 public class Update implements Runnable {
-    private Stock[] myStocks;
-    public Update(Stock[] stocks){
-        myStocks=stocks;
-    }
     public void run(){
         //Our main code
     }
