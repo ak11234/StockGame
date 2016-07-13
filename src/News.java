c66fde43a802e64a3964083c5918f3a8c3c8b111
Joshua <joshuagross4444@gmail.com>
2016-07-13 14:18:16 -0500

    started arrays

--- src/News.java
+++ src/News.java
@@ -10,6 +10,7 @@ public class News {
     private ArrayList<GLabel> myLabels;
     private int myX, myY, myAmt;
     private Game myGame;
+    private String[] position, company, effect; 
 
     public News(int xstart, int ystart, int amt, Game game) {
         myX = xstart;
@@ -22,12 +23,59 @@ public class News {
             myGame.add(myLabels.get(k));
         }
     }
+
     public void add(String text){
-       for (int k = myAmt-1 ; k>0 ; k--){
-           GLabel temp1 =myLabels.get(k);
-           GLabel temp2 = myLabels.get(k-1);
-           temp1.setLabel(temp2.getLabel());
-       }
-       myLabels.get(0).setLabel(text);
+        for (int k = myAmt-1 ; k>0 ; k--){
+            GLabel temp1 =myLabels.get(k);
+            GLabel temp2 = myLabels.get(k-1);
+            temp1.setLabel(temp2.getLabel());
+        }
+        myLabels.get(0).setLabel(text);
+        add(new GLabel("Hello"),500,300);
+
+        position = new String[10];
+        company = new String[30];
+        effect = new String[10];
+
+        position[0]="Communications Department";
+        position[1]="CEO";
+        position[2]="CFO";
+        position[3]="HR Department";
+        position[4]="Research Department";
+        position[5]="Sales Manager";
+        position[6]="Chief Sales Advisor";
+        position[7]="President";
+        position[8]="Board of Directors";
+        position[9]="Vice President";
+
+        company[0]="Apple";
+        company[1]="American Express";
+        company[2]="Boeing";
+        company[3]="Caterpillar";
+        company[4]="Merck";
+        company[5]="Microsoft";
+        company[6]="Nike";
+        company[7]="Pfizer";
+        company[8]="Cisco ";
+        company[9]="Chevron";
+        company[10]="Coca-Cola";
+        company[12]="3M Company ";
+        company[13]="IBM";
+        company[14]="Intel ";
+        company[15]="McDonald's";
+        company[16]="Johnson & Johnson";
+        company[17]="JPMorgan Chase";
+        company[18]="Disney";
+        company[19]="Wal-Mart";
+        company[20]="Travelers";
+        company[0]="UnitedHealth";
+        company[0]="ExxonMobil";
+        company[0]="Verizon";
+        company[0]="Visa";
+        company[0]="United Technologies";
+        company[0]="General Electric";
+        company[0]="Home Depot";
+        company[0]="Goldman Sachs";
+        company[0]="Procter & Gamble";
     }
 }
